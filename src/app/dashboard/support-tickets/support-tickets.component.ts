83396b4465b3346140586665022a54c77352274e
serotonine <ks.serotonine@gmail.com>
2024-09-27 15:48:17 +0200

    Master: Ticket component.

--- src/app/dashboard/support-tickets/support-tickets.component.ts
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './support-tickets.component.html',
-  styleUrl: './support-tickets.component.css'
+  styleUrl: './support-tickets.component.css',
 })
-export class SupportTicketsComponent {
-
-}
+export class SupportTicketsComponent {}
